9c1230b133b28fac24b269c3bfd1f02a465d632b
Vishali .P <vaishuvishali55@gmail.com>
2026-01-31 08:42:56 +0000

    Try fixing this error: `Build Error: Export GenkitProvider doesn't exist

--- src/components/genkit-provider.tsx
+++ src/components/genkit-provider.tsx
@@ -1,6 +1,5 @@
 'use client';
-import {GenkitProvider as Provider} from '@genkit-ai/next/client';
 
 export function GenkitProvider({children}: {children: React.ReactNode}) {
-  return <Provider>{children}</Provider>;
+  return <>{children}</>;
 }
